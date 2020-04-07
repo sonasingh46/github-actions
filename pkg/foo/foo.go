3759aa305bfbbdae66bb7dae15070174537b3c69
Ashutosh Kumar <sonasingh46@gmail.com>
2020-04-08 01:51:33 +0530

    fix(go_format): add go format check in githu actions
    Signed-off-by: Ashutosh Kumar <sonasingh46@gmail.com>

--- pkg/foo/foo.go
+++ pkg/foo/foo.go
@@ -1,6 +1,11 @@
+/*
+Copyright (c) 2020-present Author(sonasingh46@gmail.com). All Rights Reserved.
+See LICENSE.txt for license information.
+*/
+
 package foo
 
 // IsEven returns true if the provided number is even else returns false.
-func IsEven(number int)bool  {
-	return (number%2)==0
+func IsEven(number int) bool {
+	return (number % 2) == 0
 }
